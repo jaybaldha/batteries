ecc4fe199c70fcf74d58c258669721ad120a0c9f
jaybaldha <jbaldha8@gmail.com>
2020-08-12 07:16:46 -0400

    Created Json file Plugin created for navigation.

---
+++ js/highlightmenu.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#nav_menu').highlightMenu({
+        color: 'rgb(98, 161, 206)',
+        hoverColor:'rgb(56, 231, 202)',
+    });
+});
--- plugins/jquery.highlight.js
+++ plugins/jquery.highlight.js
@@ -16,21 +16,19 @@
 					'margin-bottom:10px',
 					'font-family: Cambria, Cochin, Georgia, Times',
 					'padding: 30px',
-					'color: rgb(98, 161, 206)',
 					'margin-left:0',
 					'margin-right:0',
 					'text-decoration:none',
-					'width:20%')
+					'width:20%',
+					)
 				.css('color', o.color)
 				.css('width', o.linkWidth)
 			items.mouseover(function () {
 				$(this)
 					.css('color', o.hoverColor)
-					.css('text-shadow:0.2em 0.1em #01081f')
-					.css('font-style: italic')
 			});
 			items.mouseout(function () {
-				$(this).css('color', o.color);
+				$(this).css('color', o.color)
 			});
 		});
 	};
