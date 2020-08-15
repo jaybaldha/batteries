9fee6cbd787bbad65d97f21e78c658d7961e086a
jaybaldha <jbaldha8@gmail.com>
2020-08-14 22:59:07 -0400

    Json part check

--- js/reader.js
+++ js/reader.js
@@ -1,11 +1,20 @@
-    // $(document).ready(function(){
-    //    $.getJSON("data/products.json", function(data) {
-    //        $.each(data, function(p) {
-    //            console.log(p);
-    //         //   var tblRow = "<tr>" + "<td>" + p.Name + "</td>" +
-    //         //    "<td>" +  + "</td>" + "<td>" + f.Department + "</td>" + "<td>" + f.Bio + "</td>" + "</tr>"
-    //         //    $(tblRow).appendTo("#userdata tbody");
-    //      });
-
-    //    	});
-    // });
+"use strict"
+$(document).ready(function () {
+    $.getJSON("data/products.json", function (data) {
+                console.log(data); //data is an object that goes and return with the info
+        $.each(data, function () { //data holds the item in the json file
+            $.each(this, function (key, value) {  //take each item and format it 
+                // trails for my validation
+                //var key1 = key;
+                //var value1 = value;
+                $("#ProductData").append(
+                    //  parssing JSON format to HTML
+                     
+                     '<div>' +
+                              '<h3>' + value.Name + '</h3>' +
+                     '</div>'
+                ); //end the append
+            }); //end of the second each
+        }); //end of the first each
+    }); // end getJSON
+}); //end ready
